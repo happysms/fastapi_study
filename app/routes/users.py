cee9b58790019522ba6ee916f79afcd140abd106
minsung <khuminsung@khu.ac.kr>
2021-12-16 14:24:46 +0900

    sqlalchemy wrapper 적용

--- app/routes/users.py
+++ app/routes/users.py
@@ -8,13 +8,21 @@ router = APIRouter()
 
 
 @router.get("/me", response_model=UserMe)
-async def get_user(request: Request):
+async def get_me(request: Request):
     """
     get my info
     :param request:
     :return:
     """
     user = request.state.user
-    user_info = Users.get(id=user.id)
-
+    #     # user_info = Users.get(id=user.id)
+    user_info = Users.filter(id__gt=user.id).order_by("email").count()
+    # user_info = session.query(Users).filter(Users.id > user.id).order_by(Users.email.asc()).count()
     return user_info
+
+
+@router.put('/me')
+async def put_me(request: Request):
+    ...
+
+
